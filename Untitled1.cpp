b4e13009d76cbfd2a2e776fd298b8d1de113e1fa
Bendi Dinesh <84494697+20A91A0569@users.noreply.github.com>
2021-12-13 21:15:01 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class sample
+{
+	private:
+		int x,y;
+	public:
+		void get()
+		{
+			cout<<"enter x,y values:";
+			cin>>x>>y;
+			}	
+			friend void sum(sample &s);
+};
+void sum(sample &s)
+{
+	cout<<"sum of x and y="<<s.x+s.y;
+}
+int main()
+{
+	sample s;
+	s.get();   
+	sum(s);
+	return 0;
+}
